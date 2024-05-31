44b99809141fc8151c642d54997171196d338b14
Marc Altmann <marcaltmann@posteo.de>
2024-05-31 12:30:24 +0200

    Extract file manage.py

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,16 +1,6 @@
-import os
-from pathlib import Path
-import django
-from django.conf import settings
-from django.http import HttpResponse
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.management import execute_from_command_line
 from django.shortcuts import render
-
-# Load settings.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
 
 
 def index(request):
@@ -22,6 +12,3 @@ urlpatterns = [
 ]
 
 application = WSGIHandler()
-
-if __name__ == "__main__":
-    execute_from_command_line()
