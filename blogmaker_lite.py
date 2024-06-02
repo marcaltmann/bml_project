6b0e9a134230e8f25d654cc86d48c52200359c7c
Marc Altmann <marcaltmann@posteo.de>
2024-06-02 20:23:27 +0200

    Add blogs page

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,14 +1,28 @@
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
 from django.shortcuts import render
+from django.contrib import admin
+
+from blogs.models import Blog, BlogPost
+
+
+admin.site.register((Blog, BlogPost))
 
 
 def index(request):
     return render(request, "index.html")
 
 
+def blogs(request):
+    all_blogs = Blog.objects.all()
+    context = {"blogs": all_blogs}
+    return render(request, "blogs.html", context)
+
+
 urlpatterns = [
-    path('', index),
+    path('admin/', admin.site.urls),
+    path('blogs/', blogs, name='blogs'),
+    path('', index, name='index'),
 ]
 
 application = WSGIHandler()
--- settings.py
+++ settings.py
@@ -1,24 +1,48 @@
 from pathlib import Path
 
-ROOT_URLCONF="blogmaker_lite"
+ROOT_URLCONF = "blogmaker_lite"
 
-DEBUG=True
+DEBUG = True
 
-SECRET_KEY="my-secret-key"
+SECRET_KEY = "my-secret-key"
 
-TEMPLATES=[
+TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [Path(__file__).parent / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        }
     }
 ]
 
-INSTALLED_APPS=["blogs"]
+INSTALLED_APPS = [
+    "blogs",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
 
-DATABASES={
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': Path(__file__).parent / 'db.sqlite3',
     }
 }
-DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "static/"
